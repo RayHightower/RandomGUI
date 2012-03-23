383c31c67ee9a647d3c0347e67ff6ce8bd8bc192
RayHightower <rth@wisdomgroup.com>
2012-03-22 23:25:15 -0500

    Initial Commit

---
+++ RandomNumber/WGAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  WGAppDelegate.h
+//  RandomNumber
+//
+//  Created by Raymond T. Hightower on 3/22/12.
+//  Copyright (c) 2012 WisdomGroup. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class WGViewController;
+
+@interface WGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) WGViewController *viewController;
+
+@end
---
+++ RandomNumber/WGViewController.h
@@ -0,0 +1,13 @@
+//
+//  WGViewController.h
+//  RandomNumber
+//
+//  Created by Raymond T. Hightower on 3/22/12.
+//  Copyright (c) 2012 WisdomGroup. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WGViewController : UIViewController
+
+@end
